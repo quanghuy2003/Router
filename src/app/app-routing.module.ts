e6043c6f3878cb2852a10c2221331e26a1548a9f
quanghuy2002 <hoangqunghuy753@gmail.com>
2022-01-18 16:25:22 +0700

    bài th2 Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
+import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {
+    path: 'youtube',
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
