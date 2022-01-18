6b2a311c266ee382666a7b1031eed4f1c1dd969c
quanghuy2002 <hoangqunghuy753@gmail.com>
2022-01-18 16:41:44 +0700

    bài th3 Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,39 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
+// import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
+//
+//
+// const routes: Routes = [
+//   {
+//     path: 'youtube',
+//     children: [{
+//       path: ':id',
+//       component: YoutubePlayerComponent
+//     }]
+//   }
+// ];
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
-import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
+import { Routes, RouterModule } from '@angular/router';
+import {ProductListComponent} from './product/product-list/product-list.component';
+import {ProductCreateComponent} from './product/product-create/product-create.component';
 
 
-const routes: Routes = [
-  {
-    path: 'youtube',
-    children: [{
-      path: ':id',
-      component: YoutubePlayerComponent
-    }]
-  }
-];
+const routes: Routes = [{
+  path: 'product/list',
+  component: ProductListComponent
+}, {
+  path: 'product/create',
+  component: ProductCreateComponent
+}];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
 import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     YoutubePlaylistComponent,
-    YoutubePlayerComponent
+    YoutubePlayerComponent,
+    ProductListComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
